55cae26d3f23ed687c2eb01821431f914bd4a635
GilangRizaltin <gilangmrizaltin@gmail.com>
2023-12-19 06:53:14 +0700

    updates implementation

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+export function movieCard(props) {
+    return (
+        <div className="flex flex-col gap-y-4">
+            <div className="w-full h-full rounded-md relative">
+                {props.movie_cover ? 
+                (<img
+                src={props.movie_cover}
+                alt="movie"
+                className="w-full h-full"
+              />) : 
+              (<img
+                src={getImageUrl("movie1", "png")}
+                alt="movie"
+                className="w-full h-full"
+              />)}
+              <div className="absolute w-full h-full flex flex-col gap-y-3 justify-center items-center inset-0 bg-black opacity-0 hover:opacity-100 hover:opac bg-opacity-0 hover:bg-opacity-40">
+                <Link
+                  to={"/movie/" + props.id}
+                  className="text-light text-sm p-3 w-40 border border-light rounded-md text-center focus:ring-2 "
+                >
+                  Details
+                </Link>
+                <Link
+                  to={"/movie/" + props.id}
+                  className="text-light text-sm p-3 w-40 rounded-md text-center bg-primary focus:ring-2 "
+                >
+                  Buy Ticket
+                </Link>
+              </div>
+            </div>
+            <p className="text-2xl text-dark font-semibold">{props.movie_name}</p>
+            <div className="flex flex-row gap-x-2">
+              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
+                Action
+              </p>
+              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
+                Adventure
+              </p>
+            </div>
+          </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "../styles/main.css";
@@ -6,9 +6,65 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import getImageUrl from "../utils/imageGetter";
 import DropdownMobile from "../components/DropdownMobile";
+import { getAllMovie } from "../utils/https/home";
+import { useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { movieCard } from "../components/MovieCard";
 
 function Home() {
   const [isDropdownShown, setIsDropdownShow] = useState(false);
+  const [movie, setDataMovie] = useState([])
+  const token = useSelector(state => state.user.userInfo.token)
+  const [searchParams, setSearchParams] = useSearchParams({});
+  const getMovieUrl = import.meta.env.VITE_BACKEND_HOST + "/movie"
+  useEffect(() => {
+    getAllMovie(token, getMovieUrl)
+    .then((res) => {
+      console.log(res)
+      setDataMovie(res.data.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [])
+  const search = (e) => {
+    e.preventDefault()
+    const url = import.meta.env.VITE_BACKEND_HOST + "/movie?movie_name=" + e.target.search_movie.value
+    setSearchParams({
+      movie_name : e.target.search_movie.value
+    })
+    if (!e.target.search_movie.value) {
+      setSearchParams({})
+    }
+    getAllMovie(token, url)
+    .then((res) => {
+      console.log(res)
+      setDataMovie(res.data.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      setDataMovie([])
+    })
+  }
+  const submitGenre = (e) => {
+    e.preventDefault()
+    setSearchParams((prev) => {
+      return {
+        ...prev,
+        movie_genre: e.target.value,    
+      }
+    })
+    const genreUrl = import.meta.env.VITE_BACKEND_HOST + "/movie?" + searchParams.toString()
+    getAllMovie(token, genreUrl)
+    .then((res) => {
+      console.log(res)
+      setDataMovie(res.data.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      setDataMovie([])
+    })
+  }
   return (
     <>
       <Navbar isClick={() => setIsDropdownShow(true)} />
@@ -30,167 +86,57 @@ function Home() {
         <div className="flex flex-col gap-y-5 lg:flex-row lg:gap-x-5">
           <div className="flex flex-col gap-y-4">
             <p className="text-secondary font-semibold">Cari Event</p>
-            <div className="relative">
+            <form onSubmit={search} className="relative">
               <input
                 type="text"
+                name="search_movie"
                 className="border border-[#DEDEDE] py-4 md:py-5 pl-16 pr-[18px] w-full md:w-[340px] rounded-sm outline-none placeholder:text-[#A0A3BD] placeholder:max-sm:text-sm"
                 placeholder="New Born Expert"
               />
-              <img
-                src={getImageUrl("Search", "svg")}
-                alt="icon"
-                className="absolute top-5 md:top-6 left-5"
-              />
-            </div>
+              <button type="submit">
+                <img
+                  src={getImageUrl("Search", "svg")}
+                  alt="icon"
+                  className="absolute top-5 md:top-6 left-5"
+                />
+              </button>
+            </form>
           </div>
           <div className="flex flex-col gap-y-4 lg:gap-y-7">
             <p className="text-secondary font-semibold">Filter</p>
             <div className="flex flex-col md:flex-row md:gap-x-8 md:items-center text-sm font-nunito font-semibold">
-              <p className="py-2 px-4 bg-primary text-light rounded-[10px] cursor-pointer">
+              <p className="py-2 px-4 bg-primary text-light rounded-[10px] cursor-pointer" onClick={submitGenre}>
                 Thriller
               </p>
-              <p className="py-2 px-4 text-secondary cursor-pointer">Horror</p>
-              <p className="py-2 px-4 text-secondary cursor-pointer">
+              <p className="py-2 px-4 text-secondary cursor-pointer" onClick={submitGenre}>Horror</p>
+              <p className="py-2 px-4 text-secondary cursor-pointer" onClick={submitGenre}>
                 Romantic
               </p>
-              <p className="py-2 px-4 text-secondary cursor-pointer">
+              <p className="py-2 px-4 text-secondary cursor-pointer" onClick={submitGenre}>
                 Adventure
               </p>
-              <p className="py-2 px-4 text-secondary cursor-pointer">Sci-Fi</p>
+              <p className="py-2 px-4 text-secondary cursor-pointer" onClick={submitGenre}>Sci-Fi</p>
             </div>
           </div>
         </div>
       </section>
       <section className="pb-[63px] px-5 md:px-11 xl:px-[130px] font-mulish">
         <div className="grid md:grid-cols-3 lg:grid-cols-4 md:gap-5">
-          <div className="flex flex-col gap-y-4">
-            <div className="w-full h-full rounded-md relative">
-              <img
-                src={getImageUrl("movie1", "png")}
-                alt="movie"
-                className="w-full h-full"
-              />
-              <div className="absolute w-full h-full flex flex-col gap-y-3 justify-center items-center inset-0 bg-black opacity-0 hover:opacity-100 hover:opac bg-opacity-0 hover:bg-opacity-40">
-                <Link
-                  to="/movie/1"
-                  className="text-light text-sm p-3 w-40 border border-light rounded-md text-center focus:ring-2 "
-                >
-                  Details
-                </Link>
-                <Link
-                  to="/movie/1"
-                  className="text-light text-sm p-3 w-40 rounded-md text-center bg-primary focus:ring-2 "
-                >
-                  Buy Ticket
-                </Link>
-              </div>
-            </div>
-            <p className="text-2xl text-dark font-semibold">Black Widow</p>
-            <div className="flex flex-row gap-x-2">
-              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
-                Action
-              </p>
-              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
-                Adventure
-              </p>
-            </div>
+        {movie ? (
+          movie.map((product, index) => (
+            <div key={index}>
+              {movieCard({
+                  id: product.Id,
+                  movie_cover: product.movie_photo,
+                  movie_name: product.movie_name,
+                })}
+                </div>
+                ))
+              ) : (
+              <div className='absolute text-xl md:text-2xl lg:text-4xl justify-center font-semibold text-input_border'>
+            No product information available.
           </div>
-          <div className="flex flex-col gap-y-4">
-            <div className="w-full h-full rounded-md relative">
-              <img
-                src={getImageUrl("movie2", "png")}
-                alt="movie"
-                className="w-full h-full"
-              />
-              <div className="absolute w-full h-full flex flex-col gap-y-3 justify-center items-center inset-0 bg-black opacity-0 hover:opacity-100 hover:opac bg-opacity-0 hover:bg-opacity-40">
-                <Link
-                  to="/details"
-                  className="text-light text-sm p-3 w-40 border border-light rounded-md text-center focus:ring-2 "
-                >
-                  Details
-                </Link>
-                <Link
-                  to="/details"
-                  className="text-light text-sm p-3 w-40 rounded-md text-center bg-primary focus:ring-2 "
-                >
-                  Buy Ticket
-                </Link>
-              </div>
-            </div>
-            <p className="text-2xl text-dark font-semibold">The Withces</p>
-            <div className="flex flex-row gap-x-2">
-              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
-                Comedy
-              </p>
-              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
-                Adventure
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-y-4">
-            <div className="w-full h-full rounded-md relative">
-              <img
-                src={getImageUrl("movie3", "png")}
-                alt="movie"
-                className="w-full h-full"
-              />
-              <div className="absolute w-full h-full flex flex-col gap-y-3 justify-center items-center inset-0 bg-black opacity-0 hover:opacity-100 hover:opac bg-opacity-0 hover:bg-opacity-40">
-                <Link
-                  to="/details"
-                  className="text-light text-sm p-3 w-40 border border-light rounded-md text-center focus:ring-2 "
-                >
-                  Details
-                </Link>
-                <Link
-                  to="/details"
-                  className="text-light text-sm p-3 w-40 rounded-md text-center bg-primary focus:ring-2 "
-                >
-                  Buy Ticket
-                </Link>
-              </div>
-            </div>
-            <p className="text-2xl text-dark font-semibold">Tenet</p>
-            <div className="flex flex-row gap-x-2">
-              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
-                Action
-              </p>
-              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
-                Sci-Fi
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-y-4">
-            <div className="w-full h-full rounded-md relative">
-              <img
-                src={getImageUrl("movie4", "png")}
-                alt="movie"
-                className="w-full h-full"
-              />
-              <div className="absolute w-full h-full flex flex-col gap-y-3 justify-center items-center inset-0 bg-black opacity-0 hover:opacity-100 hover:opac bg-opacity-0 hover:bg-opacity-40">
-                <Link
-                  to="/details"
-                  className="text-light text-sm p-3 w-40 border border-light rounded-md text-center focus:ring-2 "
-                >
-                  Details
-                </Link>
-                <Link
-                  to="/details"
-                  className="text-light text-sm p-3 w-40 rounded-md text-center bg-primary focus:ring-2"
-                >
-                  Buy Ticket
-                </Link>
-              </div>
-            </div>
-            <p className="text-2xl text-dark font-semibold">Spiderman</p>
-            <div className="flex flex-row gap-x-2">
-              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
-                Action
-              </p>
-              <p className="text-[#A0A3BD] px-5 py-2 bg-[#A0A3BD1A] rounded-[20px]">
-                Adventure
-              </p>
-            </div>
-          </div>
+        )}
         </div>
       </section>
       <section className="pb-[63px] flex gap-x-5 justify-center font-nunito font-medium">
